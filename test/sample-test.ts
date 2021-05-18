f2eeb1a4f24d180a7934e5188826575ad5197ca5
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-05-19 01:53:40 +0530

    pass test for managaer of factory contract

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,17 +1,30 @@
-import { ethers } from "hardhat";
-import { Signer } from "ethers";
-import {expect} from  'chai'
+import { ethers } from 'hardhat';
+// import { Signer } from "ethers";
+import { expect } from 'chai';
 
-describe("PostFactory", () => {
-  let accounts: Signer[]
+describe('PostFactory', () => {
+  // let accounts: Signer[]
+  // @ts-ignore
+  let PostFactory, postFactory: any, owner, addr1, addr2;
 
   beforeEach(async () => {
-    accounts = await ethers.getSigners()
-  })
-  it("check manager of the Post Factory", async () => {
-    const PostFactory = await ethers.getContractFactory('PostFactory')
-    let postFactory = await PostFactory.deploy()
+    // accounts = await ethers.getSigners();
+    // console.log(accounts);
+    
+    PostFactory = await ethers.getContractFactory('PostFactory');
+    postFactory = await PostFactory.deploy();
+    [owner, addr1, addr2] = await ethers.getSigners();
+    // console.log(addr1.address);
+    console.log('owner: ',owner.address)
+    
+  });
+  it('check manager of the Post Factory', async () => {
+    const manager = await postFactory.manager()
 
-    expect(await postFactory.manager()).to.equal(accounts[0])
-  })
-})
+    console.log(manager);
+    // @ts-ignore
+    expect(await postFactory.manager()).to.equal(owner.address);
+    
+    
+  });
+});
