a788f8d28acf40f92afa4ef5bf9bcfefdd0d09c4
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-05-19 17:14:50 +0530

    access post from the given deployed address

--- test/post-test.ts
+++ test/post-test.ts
@@ -1,17 +1,19 @@
 import { ethers } from 'hardhat';
-// import { Signer } from "ethers";
+import { Signer } from "ethers";
 import { expect } from 'chai';
 
 describe('PostFactory', () => {
-  // let accounts: Signer[]
+  let accounts: Signer[]
   // @ts-ignore
-  let PostFactory, postFactory: any, owner, addr1, addr2, username: string;
+  let PostFactory, postFactory: any, owner, addr1, addr2, username: string, Post;
 
   beforeEach(async () => {
-    // accounts = await ethers.getSigners();
+    accounts = await ethers.getSigners();
     // console.log(accounts);
 
     PostFactory = await ethers.getContractFactory('PostFactory');
+    Post = await ethers.getContractFactory('Post');
+    
     postFactory = await PostFactory.deploy();
     [owner, addr1, addr2] = await ethers.getSigners();
     // console.log(addr1.address);
@@ -19,6 +21,7 @@ describe('PostFactory', () => {
     username = 'user1';
     await postFactory.registerUser(username);
   });
+
   it('check manager of the Post Factory', async () => {
     const manager = await postFactory.manager();
     console.log(manager);
@@ -35,6 +38,7 @@ describe('PostFactory', () => {
   });
 
   it('Deploy Post', async() => {
+
     const title = 'title'
     const body = 'body'
 
@@ -45,8 +49,16 @@ describe('PostFactory', () => {
     expect(length.toNumber()).to.equal(1);
 
     const [postAddress] = await postFactory.getDeployedPosts();
-    console.log('postAddress: ', postAddress)
+    // console.log('postAddress: ', postAddress)
     expect(postAddress).to.not.be.null;
+
+
+    // @ts-ignore
+    // console.log('Post: ', Post.interface);
+    // @ts-ignore
+    const post = await new ethers.Contract(postAddress, Post.interface, accounts[0]);
+
+    // console.log('Post: ', post);
     
   })
 });
